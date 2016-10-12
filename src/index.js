fd9e44d3de975d5f9c52cc64d38eb6f5e0262fc3
karol.brozek <kbro29@gmail.com>
2016-10-12 16:41:24 +0200

    react-app       index.js       App.js       Maps.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Maps from './maps/Maps';
+
 import './index.css';
 
 import { Router, Route, browserHistory } from 'react-router'
 
 ReactDOM.render(
     <Router history={browserHistory}>
-     <Route path="/" component={App} />
+        <Route path="/" component={App} />
+        <Route path="/maps" component={Maps} />
     </Router>,
     document.getElementById('root')
 );
---
+++ src/maps/Maps.js
@@ -0,0 +1,12 @@
+/**
+ * Created by karol on 12.10.16.
+ */
+import React from 'react'
+import $ from 'jquery'
+
+import { Link } from 'react-router'
+
+export default class Counter extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
