9f47ef6deed2830a44c4e6d1f11ef4ad080e2e36
Beata Kossakowska <bbkossakowska@gmail.com>
2016-10-12 16:59:12 +0200

    dodanie linkow products/shops

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Products from './products/Products';
+import Shops from './shops/Shops'
+
+import { Router, Route, Link, browserHistory } from 'react-router'
 
 ReactDOM.render(
-  <App />,
+    <Router history={browserHistory}>
+        <Route path="/" component={App} />
+        <Route path="/products" component={Products}/>
+        <Route path="/shops" component={Shops}/>
+  </Router>,
   document.getElementById('root')
 );
