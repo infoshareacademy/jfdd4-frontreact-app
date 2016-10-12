aa2582a347d163481c6d1d25240a327f1b92486d
karol.brozek <kbro29@gmail.com>
2016-10-12 14:54:41 +0200

    react-app index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 ReactDOM.render(
     <Router history={browserHistory}>
