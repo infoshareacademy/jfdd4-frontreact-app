08918d249cfde3ca5f42a76a216164a4470d0d1d
Patryk Huzarski <phuzarski@gmail.com>
2016-10-13 17:00:10 +0200

    List of products is showed in website

--- src/products/Products.js
+++ src/products/Products.js
@@ -1,4 +1,31 @@
-
 import React from 'react'
+import products from '../data/dataProducts'
 
-export default (props) => <h1>Products</h1> 
+// export default (props) => <h1>Products</h1>
+
+export default class Products extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            products: []
+        }
+    }
+
+
+componentWillMount() {
+
+}
+
+render() {
+    return (
+        <ul>
+            <h1>Products</h1>
+            {products.map(function (products) {
+                return <li>{products.ID}{products.Name}{products.Price}{products.Shops}</li>
+            })}
+        </ul>
+    )
+}
+
+}
