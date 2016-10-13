9b4d8fde1853b452322febc587688467051432a5
Piotr Szablewski <piotr.szablewski@vp.pl>
2016-10-13 15:55:19 +0200

    chang name of directory list/data  updating shops

---
+++ src/data/shops.js
@@ -0,0 +1,62 @@
+/**
+ * Created by piotrszablewski on 12.10.16.
+ */
+
+
+
+var shops = {
+    data: [
+        {
+            id : 1,
+            name : 'Lidl', 
+            location: {lat:54.356504,lng:18.588442},
+            opened : 'pn-nd od 8:00 do 21:00'
+        },
+
+    
+
+    
+        {
+            id : 2,
+            name : 'Biedronka',
+            location: {lat:54.342656,lng:18.615378},
+            opened : 'pn-nd od 8:00 do 21:00'
+        },
+       
+
+
+
+
+
+   
+        {
+            id: 3,
+            name: 'Lidl',
+            location: {lat:54.354616, lng:18.638935},
+            opened: 'pn-nd od 6:00 do 24:00'
+        },
+
+
+        {
+            id: 4,
+            name: 'Stokrotka',
+            location: {lat:54.356504,lng:18.588442},
+            opened: 'pn-nd od 76:00 do 24:00'
+        },
+
+
+        {
+            id: 5,
+            name: 'U Jadzi',
+            location: {lat:54.354616,lng:18.638935},
+            opened: 'pn-nd od 76:00 do 24:00'
+        }
+
+
+        
+
+
+
+    ]
+};
+export default shops
