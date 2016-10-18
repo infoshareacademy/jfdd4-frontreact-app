9e5dcd20505715868a3a70abc04478ffbc880c22
patryk <phuzarski@gmail.com>
2016-10-18 23:09:24 +0200

    localStorrage favorites

--- src/favorites/Favorites.js
+++ src/favorites/Favorites.js
@@ -1,4 +1,29 @@
-
 import React from 'react'
 
-export default (props) => <h1>Ulubione</h1>
+
+function getFavoriteShops() {
+    return JSON.parse(localStorage.getItem('favoriteShops')) || []
+}
+
+function favoriteShops () {
+    for (var i=0; i < localStorage.length; i++) {
+    console.log(localStorage.getItem(localStorage.key(i))
+    )}
+}
+
+
+
+// var favoriteProducts = getFavoriteProducts();
+// var favoriteShops = getFavoriteShops();
+
+export default (props) => (
+            <div>
+                <h1>Favorites</h1>
+                <ul>
+                    {favoriteShops().map(function (item, index) {
+                        return <li key={index}>{item}</li>
+                    })}
+                </ul>
+            </div>
+)
+
