8d7284c07b39371cf1b05a1f4d3bde3d71780c2b
karol.brozek <kbro29@gmail.com>
2016-10-20 09:07:14 +0200

    react-app       index.js       App.js       Maps.js +++(13.10)-(14.10)+(17.10)       Login.js+19.10 dataShops edit maps edit +19.10 info css onClick info_max => MODAL
    Availability  19.10 ==> almost done
    
    Commute 20.10

--- src/maps/commute/Commute.js
+++ src/maps/commute/Commute.js
@@ -2,7 +2,7 @@
  * Created by karol on 19.10.2016.
  */
 import React from 'react'
-
+import {Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap'
 import {finalState} from '../../data/dataShops'
 
 
@@ -26,13 +26,24 @@ export default class Shops extends React.Component {
 
         return (
             <div>
-                <h1>Wybierz sklep lb sklepy do których chesz dojechać</h1>
-                <div>{this.state.shops.map(function (shops) {
-                    return <button key={shops.id}>
+                <h1>Wybierz sklep lub sklepy do których chesz dojechać</h1>
+                <ButtonToolbar>
+                    <ButtonGroup> {this.state.shops.map(function (shops) {
+                    return <Button bsStyle="primary" bsSize="large" id="button1" key={shops.id}>
                         {shops.name}
-                    </button>
-                })}
-                </div>
+                    </Button>
+                })}</ButtonGroup>
+                <ButtonGroup>
+                <Button bsStyle="primary" bsSize="large" class="button2">
+                        Twoja Pozycja
+                   </Button>
+                    </ButtonGroup>
+                    <ButtonGroup>
+                   <Button bsStyle="primary" bsSize="large" class="button2">
+                       Wyczyść
+                   </Button>
+                   </ButtonGroup>
+                </ButtonToolbar>
             </div>
         )
     }
