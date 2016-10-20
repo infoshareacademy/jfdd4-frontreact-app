3d56e9c639e2285e64f9320226594cfc08a319e8
karol.brozek <kbro29@gmail.com>
2016-10-20 15:21:50 +0200

    react-app       index.js       App.js       Maps.js +++(13.10)-(14.10)+(17.10)       Login.js+19.10 dataShops edit maps edit +19.10 info css onClick info_max => MODAL
    Availability  19.10 ==> almost done
    
    Commute 20.10

--- src/maps/commute/Commute.js
+++ src/maps/commute/Commute.js
@@ -2,10 +2,50 @@
  * Created by karol on 19.10.2016.
  */
 import React from 'react'
+import GoogleMap from 'google-map-react'
 import {Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap'
 import {finalState} from '../../data/dataShops'
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude);
+}
 
+getLocation();
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+    directionsService.route({
+        origin: 'Gdynia',
+        destination: 'Warszawa',
+        travelMode: 'DRIVING'
+    }, 
+
+        function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+
+}
+
+let handleApi = ({map, maps}) => {
+    console.log(map, maps)
+    var directionsService = new maps.DirectionsService();
+    var directionsDisplay = new maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
 
 export default class Shops extends React.Component {
     constructor() {
@@ -30,7 +70,7 @@ export default class Shops extends React.Component {
                 <ButtonToolbar>
                     <ButtonGroup> {this.state.shops.map(function (shops) {
                     return <Button bsStyle="primary" bsSize="large" id="button1" key={shops.id}>
-                        {shops.name}
+                        {shops.name} 
                     </Button>
                 })}</ButtonGroup>
                 <ButtonGroup>
@@ -44,7 +84,18 @@ export default class Shops extends React.Component {
                    </Button>
                    </ButtonGroup>
                 </ButtonToolbar>
+                <div style={{ width: '100%', height: 500 }}>
+                    <GoogleMap
+                        bootstrapURLKeys={{
+                          key: 'AIzaSyCkDbleAYeCPGyTEDJ8Jk94gwXDxombvRE'}}
+                        center={[59.938043, 30.337157]}
+                        zoom={9}
+                        onGoogleApiLoaded={handleApi}
+                        yesIWantToUseGoogleMapApiInternals>
+                    </GoogleMap>
+                </div>
             </div>
         )
     }
 }
+
