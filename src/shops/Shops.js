517b96067475161b93f02f9dd7e4c3db68d23360
Karol <kbro29@gmail.com>
2016-10-23 21:41:44 +0200

    poprawiona notatka z map, login

--- src/shops/Shops.js
+++ src/shops/Shops.js
@@ -30,7 +30,7 @@ export default class Shops extends React.Component {
                 shopsData: finalShopsState
             })
         }.bind(this), 0)
-        
+
         setTimeout(function () {
             this.setState({
                 productsData: finalProductsState
@@ -51,58 +51,70 @@ export default class Shops extends React.Component {
         return (
             <div>
                 <Well>
-                <PageHeader>Lista sklepów
-                    <small> dodaj lub usuń sklepy z obserwowanych.</small></PageHeader>
-                <p>
-                    <Link to={'/shops/with-products'}>
-                        {this.props.children}
-                        <Button bsStyle="primary">Pokaż dostępne produkty</Button>
-                    </Link>
-                    <Link to={'/shops'}>
-                        {this.props.children}
-                    <Button bsStyle="primary">Pokaż wszystkie sklepy</Button>
-                    </Link>
-                </p>
-                <Table striped bordered condensed hover>
-                    <thead>
-                    </thead>
-                    <ReactCSSTransitionGroup
-                        component="tbody"
-                        transitionName="example"
-                        transitionEnterTimeout={500}
-                        transitionLeaveTimeout={300}>
-                    {this.state.shops.map(function (shop) {
-                        return (
-                            <tr className={favourites.find(shopId => shopId === shop.id) ? 'favourite' : ''} key={shop.id}>
-                                    <td>{shop.name}</td>
-                                <Button bsStyle="success" onClick={() => {markShopAsFavorite(shop); forceUpdate(); alert("Dodano sklep do ulubionych")}}><Glyphicon glyph="glyphicon glyphicon-ok" aria-hidden="true"/></Button>
-                                <Button bsStyle="danger" onClick={() => {dissMarkShopAsFavorite(shop);forceUpdate(); alert("Usunięto sklep z ulubionych")}}><Glyphicon glyph="glyphicon glyphicon-remove" aria-hidden="true"/></Button>
-                                {viewVariant === 'with-products' ?
-                                    <ul>
-                                        {productsData.products.filter(function (product) {
-                                            for( var i = 0; i < product.shops.length; i++ ){
-                                                if (product.shops[i] === shop.id) {
-                                                    return true;
+                    <PageHeader>Lista sklepów
+                        <small> dodaj lub usuń sklepy z obserwowanych.</small>
+                    </PageHeader>
+                    <p>
+                        <Link to={'/shops/with-products'}>
+                            {this.props.children}
+                            <Button bsStyle="primary">Pokaż dostępne produkty</Button>
+                        </Link>
+                        <Link to={'/shops'}>
+                            {this.props.children}
+                            <Button bsStyle="primary">Pokaż wszystkie sklepy</Button>
+                        </Link>
+                    </p>
+                    <Table striped bordered condensed hover>
+                        <thead>
+                        </thead>
+                        <ReactCSSTransitionGroup
+                            component="tbody"
+                            transitionName="example"
+                            transitionEnterTimeout={500}
+                            transitionLeaveTimeout={300}>
+                            {this.state.shops.map(function (shop) {
+                                return (
+                                    <tr className={favourites.find(shopId => shopId === shop.id) ? 'favourite' : ''}
+                                        key={shop.id}>
+                                        <td>{shop.name}</td>
+                                        <Button bsStyle="success" onClick={() => {
+                                            markShopAsFavorite(shop);
+                                            forceUpdate();
+                                            alert("Dodano sklep do ulubionych")
+                                        }}><Glyphicon glyph="glyphicon glyphicon-ok" aria-hidden="true"/></Button>
+                                        <Button bsStyle="danger" onClick={() => {
+                                            dissMarkShopAsFavorite(shop);
+                                            forceUpdate();
+                                            alert("Usunięto sklep z ulubionych")
+                                        }}><Glyphicon glyph="glyphicon glyphicon-remove" aria-hidden="true"/></Button>
+                                        {viewVariant === 'with-products' ?
+                                            <ul>
+                                                {productsData.products.filter(function (product) {
+                                                    for (var i = 0; i < product.shops.length; i++) {
+                                                        if (product.shops[i] === shop.id) {
+                                                            return true;
+                                                        }
+                                                    }
+                                                    return false;
+                                                }).map(function (product) {
+                                                    return (
+                                                        <tr key={product.id}><Glyphicon glyph="glyphicon glyphicon-ok"
+                                                                                        aria-hidden="true"/>
+                                                            <td>{product.name}</td>
+                                                            <Glyphicon glyph="glyphicon glyphicon-remove"
+                                                                       aria-hidden="true"/>
+                                                        </tr>
+                                                    )
+                                                })
                                                 }
-                                            }
-                                            return false;
-                                        }).map(function(product) {
-                                            return (
-                                                <tr key={product.id}><Glyphicon glyph="glyphicon glyphicon-ok" aria-hidden="true"/>
-                                                    <td>{product.name}</td><Glyphicon glyph="glyphicon glyphicon-remove" aria-hidden="true"/>
-                                                </tr>
-                                            )
-                                        })
-                                        }
-                                    </ul> : null}
-                            </tr>
-                        )
-                    })}
-                    </ReactCSSTransitionGroup>
-                </Table>
-                    </Well>
+                                            </ul> : null}
+                                    </tr>
+                                )
+                            })}
+                        </ReactCSSTransitionGroup>
+                    </Table>
+                </Well>
             </div>
         )
     }
-
 }
