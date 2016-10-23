75aca152a9a2ef2b3fc45b71d4e9bb62e7025d1e
Karol <kbro29@gmail.com>
2016-10-23 22:12:41 +0200

    poprawiona notatka z map, login

--- src/app/menu/Menu.js
+++ src/app/menu/Menu.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './Menu.css'
 import Item from './item/Item'
 import { Grid, Row, Col, Well , Navbar, Nav } from 'react-bootstrap'
+import GoogleLogin from 'react-google-login';
 
 export default class Menu extends Component {
     render() {
@@ -28,9 +29,19 @@ export default class Menu extends Component {
                             </Item>
                         </Nav>
                     </Nav>
+                    <Navbar.Form pullRight>
+                        <GoogleLogin
+                            clientId="287909334341-a9d7rvs2g737tlm1vibohhn0a4kn5bj0.apps.googleusercontent.com"
+                            onSuccess={console.log}
+                            onFailure={console.log}
+                            className="btn btn-default btn-sm">
+                            <img src="/public/img/singin-icon.jpg"/>
+                        </GoogleLogin>
+                    </Navbar.Form>
                     <Nav pullRight>
                         <Nav><Item path={`/favorites`}>Ulubione</Item></Nav>
                     </Nav>
+
                 </Navbar.Collapse>
             </Navbar>
         );
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,29 @@
 /**
  * Created by karol on 12.10.16.
  */
+
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
+const mapStateToProps = (state) => ({
+    user: state.user
+})
 
-export default (props) => <h1> </h1>
+class Login extends React.Component {
+    render() {
+        return (
+
+            <GoogleLogin
+                clientId="287909334341-a9d7rvs2g737tlm1vibohhn0a4kn5bj0.apps.googleusercontent.com"
+                onSuccess={console.log}
+                onFailure={console.log}
+                style={{
+                    background: '#369',
+                    fontFamily: 'sans-serif'
+                }}
+            />
+        );
+    }
+}
+
+export default Login;
