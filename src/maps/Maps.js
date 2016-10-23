0d88047966eef47f5b8eb6791f09c841ecca730e
Karol <kbro29@gmail.com>
2016-10-23 19:34:15 +0200

    poprawiona otatka z map

--- src/maps/Maps.js
+++ src/maps/Maps.js
@@ -1,4 +1,6 @@
-
+/**
+ * Created by karol on 12.10.16.
+ */
 import React from 'react'
 import GoogleMap from 'google-map-react'
 import Place from './place/Place'
@@ -43,7 +45,7 @@ export default class Shops extends React.Component {
         return (
                 <Well>
                     <PageHeader>Mapy
-                        <small> znajdz najblizsze sklepy w Twojej okolicy.</small></PageHeader>
+                        <small> znajdź najbliższe sklepy w Twojej okolicy.</small></PageHeader>
             <div id="MAP">
                 <GoogleMap
                     bootstrapURLKeys={{key: 'AIzaSyCIGFuueBb3ewt-Ewe7ySfhE9ZdHVjdPsc'}}
@@ -56,7 +58,7 @@ export default class Shops extends React.Component {
                 </GoogleMap>
                 <Modal show={this.state.showModal} onHide={() => this.setState({ showModal: false})}>
                     <Modal.Header closeButton>
-                        <Modal.Title>{shop.name}   -   {shop.adres}</Modal.Title>
+                        <Modal.Title><h2>{shop.name}   -   {shop.adres}</h2></Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                        <Info key={shop.id}  {...shop.location} pic={shop.pic} link={shop.link} info={shop.info}
@@ -67,6 +69,7 @@ export default class Shops extends React.Component {
                         <Button onClick={() => this.setState({ showModal: false})}>Close</Button>
                     </Modal.Footer>
                 </Modal>
+
             </div>
                 </Well>
         )
