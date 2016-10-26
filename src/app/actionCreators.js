264ed2222fb44b01b7b31f98c06095e5838c16d3
Piotr Szablewski <piotr.szablewski@vp.pl>
2016-10-26 16:57:13 +0200

    adding redux to shops

--- src/app/actionCreators.js
+++ src/app/actionCreators.js
@@ -1,4 +1,4 @@
-import{ MARK_PRODUCT_AS_FAVORITE, DISSMARK_PRODUCT_AS_FAVORITE, ADD_TO_FAVORITES_BEGIN, ADD_TO_FAVORITES_END } from './actionTypes'
+import{ MARK_PRODUCT_AS_FAVORITE, DISSMARK_PRODUCT_AS_FAVORITE, ADD_TO_FAVORITES_BEGIN, ADD_TO_FAVORITES_END, ADD_SHOP_TO_FAVORITES_BEGIN } from './actionTypes'
 
 export function markProductAsFavorite(productId) {
     return {
@@ -29,11 +29,33 @@ function addToFavoritesEnd (productId) {
     }
 }
 
+function addShopToFavoritesBegin (shopId) {
+    return {
+        type: ADD_SHOP_TO_FAVORITES_BEGIN,
+        shopId: shopId
+    }
+}
+function addShopToFavoritesEnd (shopId) {
+    return {
+        type: ADD_TO_FAVORITES_BEGIN,
+        shopId: shopId
+    }
+}
+
 export function addToFavorites (productId) {
     return function (dispatch) {
         dispatch(addToFavoritesBegin(productId))
         return fetch()
             .then(response => response.json())
             .then(products => dispatch(addToFavoritesEnd(productId)))
+    }
+}
+
+export function addToFavoritesShops (shopId) {
+    return function (dispatch) {
+        dispatch(addShopToFavoritesBegin(shopId))
+        return fetch()
+            .then(response => response.json())
+            .then(shops => dispatch(addShopToFavoritesEnd(shopId)))
     }
 }
