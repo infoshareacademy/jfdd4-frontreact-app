c8c0ba954c2d0946ab5546d270ce87a0e140348b
Karol <kbro29@gmail.com>
2016-10-26 23:31:45 +0200

    product counter

--- src/shoppingList/shoppingList.js
+++ src/shoppingList/shoppingList.js
@@ -2,11 +2,31 @@
  * Created by kbro2 on 26.10.2016.
  */
 import React from 'react'
-import ReactDOM from 'react-dom';
 import {Button, Well , PageHeader, Row, Col, Image } from 'react-bootstrap'
 
 
 export default class shoppingList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            numberOfClicks: 0
+        };
+        this._handlePlusClick = this._handlePlusClick.bind(this);
+        this._handleMinusClick = this._handleMinusClick.bind(this);
+        }
+
+    _handlePlusClick() {
+        this.setState({
+            numberOfClicks: this.state.numberOfClicks + 1
+        });
+    }
+
+    _handleMinusClick() {
+        this.setState({
+            numberOfClicks: this.state.numberOfClicks - 1
+        });
+    }
+
 
 
     render() {
@@ -20,20 +40,28 @@ export default class shoppingList extends React.Component {
                         <Image src="/img/stokrotka.jpg"/>
                     </Col>
                     <Col md={3}>
-                        <Button>Hello</Button>
+                    <h1>{this.state.numberOfClicks}</h1>
+                </Col>
+                    <Col md={3}>
+                        <Button handleClick={this._handlePlusClick}>  {this.props.children}
+                           wiecej</Button>
+                    </Col>
+
+                    <Col md={3}>
+                        <Button
+                            disabled={this.props.isDisabled}
+                            onClick={this.props.handleClick}
+                            handleClick={this._handleMinusClick}>  {this.props.children}
+                            Mniej</Button>
                     </Col>
                     <Col md={3}>
-                        <Button>Hello</Button>
-                    </Col>
-                    <Col md={3}>
-                        <Button>Hello</Button>
+                        <Button type="submit" class="btn btn-default" onclick="addPost();return false;">Dodaj do listy</Button>
                     </Col>
                 </Row>
             </Well>
 
-
         )
     }
+
+
 }
-
-
