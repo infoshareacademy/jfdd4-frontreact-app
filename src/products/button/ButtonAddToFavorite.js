6c680f02ae16cb75c0a7f60fa076c1c97e65ed89
Beata Kossakowska <bbkossakowska@gmail.com>
2016-10-31 08:24:04 +0100

    adding buttons on product card

---
+++ src/products/button/ButtonAddToFavorite.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { FABButton, Icon, IconButton } from 'react-mdl';
+
+export default class ButtonAddToFavorite extends Component {
+    render() {
+        return (
+            <IconButton name="favorite" />
+        )
+    }
+};
--- src/products/button/ButtonAddToList.js
+++ src/products/button/ButtonAddToList.js
@@ -1,2 +1,17 @@
+import React, {Component} from 'react';
+import { FABButton, Icon } from 'react-mdl';
+import '../Products.css';
 
+const style = {
+    marginRight: 20,
+};
 
+export default class ButtonAddToList extends Component {
+    render() {
+        return (
+            <FABButton mini colored ripple ClassName="mdl-button--floating-action">
+                <Icon name="add"/>
+            </FABButton>
+        )
+    }
+};
